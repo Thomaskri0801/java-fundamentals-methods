8ac0e0d59679a557d1c1ba3302dc3d4d6529439e
Thomas Kristiansen <thomaskri0801@hotmail.com>
2025-01-07 15:29:48 +0100

    core tasks done

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -35,7 +35,7 @@ public class Exercise extends ExerciseBase {
         Complete this method so that it increases the number given by 1 and returns the result
      */
     public int increment(int number) {
-        return 0;
+        return number + 1;
     }
 
     /*
@@ -48,8 +48,8 @@ public class Exercise extends ExerciseBase {
         Nathan | Hi, Nathan :)
         Edward | Hi, Edward :)
      */
-    public String happilyGreet() {
-        return "Not implemented yet";
+    public String happilyGreet(String name) {
+        return "Hi, " + name + " :)";
     }
 
     /*
@@ -64,7 +64,14 @@ public class Exercise extends ExerciseBase {
         10, 13 | [10,11,12,13]
         -1, 1  | [-1,0,1]
      */
-
+    public int[] constructNumberArray(int lower, int upper) {
+        int[] result = new int[upper - lower + 1];
+        for(int i = 0; i < result.length; i++) {
+            result[i] = lower;
+            lower++;
+        }
+        return result;
+    }
 
 
 
@@ -80,7 +87,13 @@ public class Exercise extends ExerciseBase {
         disaster, 5 | DISASTER!!!!!
         error, 10   | ERROR!!!!!!!!!!
      */
-
+    public String shout(String shout, int num) {
+        String exclamation = "";
+        for (int i = 0; i < num; i++) {
+            exclamation += "!";
+        }
+        return shout.toUpperCase() + exclamation;
+    }
 
 
 
